e808ae54344d9dd08da871e966500f882133e875
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-03-02 23:16:06 +0300

    :construction: Added inputs

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-console.log('Hello World');
-console.log("no wayy");
+let hamItems = {
+  lettuce: 5,
+  souce: 5,
+  onion: 5,
+  meatball: 5,
+  chicken: 5,
+  bread: 5,
+  tomato: 5,
+  pateto: 5,
+  coke: 5,
+}
