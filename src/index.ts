25baf4a51cb7e4ad82e52bc1e1abe8a9f179d06c
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-03-03 16:39:04 +0300

    :construction: Added an order func
    Taking an order and checking if there no enough item throw an error

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-let hamItems = {
+interface ItemConfig {
+  lettuce: number;
+  souce: number;
+  onion: number;
+  meatball: number;
+  chicken: number;
+  bread: number;
+  tomato: number;
+  pateto: number;
+  coke: number;
+}
+
+let hamItems: ItemConfig = {
   lettuce: 5,
   souce: 5,
   onion: 5,
@@ -9,3 +21,37 @@ let hamItems = {
   pateto: 5,
   coke: 5,
 }
+
+// const makeAPromise = (item:string,time:number)=>{
+//   return new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//       resolve(item);
+//     },time);
+//   });
+// }
+
+// INPUTS 
+let orderObj = {
+  isChicken: false,
+  isMeatball: false,
+  isLettuce: false,
+  isTomato: false,
+  isOnion: false,
+  isSouce: false,
+  isPateto: false,
+  isBread: false,
+  isCoke: false,
+}
+const takeAnOrder = (time: number, items: ItemConfig, work: Function) => {
+  return new Promise((resolve, reject) => {
+    if (Object.values(items).every((item) => item > 0)) {
+      setTimeout(() => {
+        resolve(work(items));
+      }, time);
+    } else {
+      reject('Not Enough Items');
+    }
+  });
+}
+
+takeAnOrder(1000, hamItems, () => { console.log('Order Placed') })
